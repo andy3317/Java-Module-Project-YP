bb4d2a708791b6c5df2f08fe2017bb426dce73ac
Andrey Godun <arizona3317@gmail.com>
2024-11-12 22:52:34 +0300

    Андрей Годун - готовое первое Проектное Задание про гонку автомобилей.

---
+++ src/main/java/Automobile.java
@@ -0,0 +1,9 @@
+public class Automobile {
+    String autoBrand;
+    int autoSpeed;
+
+    public Automobile(String autoBrand, int autoSpeed ){
+        this.autoBrand = autoBrand;
+        this.autoSpeed = autoSpeed;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,49 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
+    static ArrayList<Automobile> listOfAuto = new ArrayList<>();
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Получаем список участников
+        for (int i = 1; i <= 3; i++) {
+            GetUserData(i);
+        }
+        // Вычисляем победителя и выводим результат
+        WhoIsWinner winner = new WhoIsWinner(listOfAuto);
+        winner.ThisIsAWinner();
+        System.out.println("Победитель этой непростой гонки автомобиль " + winner.ThisIsAWinner() +"!\n" +
+                           " Поздравляем нового Чемпиона!!!");
+
     }
+
+    public static void GetUserData(int member) {
+        String autoBrand;
+        int autoSpeed;
+
+        System.out.print("Введите марку машины - " + member + "-ого участника соревнований: ");
+        autoBrand = scanner.nextLine();
+
+        System.out.print("Введите скорость машины: ");
+        while(true) {
+            autoSpeed = scanner.nextInt();
+            if ((autoSpeed > 0) && (autoSpeed <= 250)) {
+                break;
+            } else {
+                System.out.print("Вы ввели неверную скорость. Введите скорость машины в пределах от 0 до 250 включительно: ");
+            }
+
+        }
+
+        // Так как scanner.nextInt() не читает конец строки, добавляем команду,
+        // Чтобы не было пропусков запросов.
+        scanner.nextLine();
+        // Добавляем участника в список
+        listOfAuto.add(new Automobile( autoBrand, autoSpeed ));
+
+    }
+
 }
+
